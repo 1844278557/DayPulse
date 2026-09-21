91b72e1829447c43acabf6e190e2f8feda4294a4
BIN <128377371+1844278557@users.noreply.github.com>
2026-09-21 18:31:27 +0800

    test: include AndroidX permission rules to prevent startup dialog hijacking Compose tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test:runner:1.7.0")
+    androidTestImplementation("androidx.test:rules:1.7.0")
 }
